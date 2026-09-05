c5e9dfc3e8e222c2a7bdecd36432333d99766ff2
verishajir <verishajir@gmail.com>
2026-09-05 00:52:54 +0000

    Initial commit: src/Engine/Domain/Contracts/IKeyDerivationService.cs

---
+++ src/Engine/Domain/Contracts/IKeyDerivationService.cs
@@ -0,0 +1,14 @@
+namespace Warframe.Engine.Domain.Contracts;
+
+public interface IKeyDerivationService
+{
+    byte[] DeriveMasterSeed(string mnemonic, string? passphrase);
+
+    byte[] DerivePrivateKey(byte[] masterSeed, string derivationPath);
+
+    string DerivePublicAddress(byte[] privateKey, string chainType);
+
+    bool ValidateMnemonic(string mnemonic);
+
+    string NormalizeDerivationPath(string path, string networkPrefix);
+}
