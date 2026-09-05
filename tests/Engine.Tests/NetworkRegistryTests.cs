8f5ae831e8ca61ec1f3894bfb3e88b05796895f4
verishajir <verishajir@gmail.com>
2026-09-05 00:50:18 +0000

    Initial commit: tests/Engine.Tests/NetworkRegistryTests.cs

---
+++ tests/Engine.Tests/NetworkRegistryTests.cs
@@ -0,0 +1,24 @@
+using Warframe.Engine.Networks;
+using Xunit;
+
+namespace Warframe.Engine.Tests;
+
+public class NetworkRegistryTests
+{
+    [Fact]
+    public void GetNetwork_ReturnsEthereumDescriptor()
+    {
+        var registry = new NetworkRegistry();
+        var network = registry.GetNetwork("ethereum-mainnet");
+
+        Assert.Equal("ETH", network.NativeAssetSymbol);
+        Assert.Equal("evm", network.ChainType);
+    }
+
+    [Fact]
+    public void GetAllNetworks_ReturnsAtLeastFourNetworks()
+    {
+        var registry = new NetworkRegistry();
+        Assert.True(registry.GetAllNetworks().Count >= 4);
+    }
+}
